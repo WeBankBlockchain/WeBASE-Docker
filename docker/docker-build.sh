e771081f139def3f9b354c651dac3a988d57de23
yanyanho <yanyanho@126.com>
2020-03-30 15:18:38 +0800

    fix the docker-build

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -3,12 +3,13 @@
 # wget https://www.fisco.com.cn/cdn/webase/releases/download/v1.2.3/webase-front.zip
 
  git clone -b $1 https://github.com/WeBankFinTech/WeBASE-Front.git && cd  WeBASE-Front
-  ./gradlew build -x test
+ ./gradlew build -x test
   cp -r dist ../
- cp ./start.sh ./dist/
- cp -r ./dist/conf_template ./dist/conf
+  cd ..
+  cp ./start.sh ./dist/
+  cp -r ./dist/conf_template ./dist/conf
 
  echo "此次镜像tag: $2"
- docker build -t  front:$2 .
- docker tag  front:$2 fiscoorg/front:$2
+# docker build -t  front:$2 .
+# docker tag  front:$2 fiscoorg/front:$2
 # docker push fiscoorg/front:$1
