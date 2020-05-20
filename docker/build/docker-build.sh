fec1abec5f3e3d6776a0ebdc1c4e9e31ff4fbf0a
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2020-05-20 15:11:18 +0800

    delete guomi in gradle build command

--- docker/build/docker-build.sh
+++ docker/build/docker-build.sh
@@ -29,14 +29,14 @@ trap 'echo -e "Aborted, error $? in command: $BASH_COMMAND"; trap ERR;  exit 1' 
 
 # Set magic variables for current file & dir
 # 脚本所在的目录
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # 脚本的全路径，包含脚本文件名
-__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+#__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 # 脚本的名称，不包含扩展名
-__base="$(basename ${__file} .sh)"
+#__base="$(basename ${__file} .sh)"
 # 脚本所在的目录的父目录，一般脚本都会在父项目中的子目录，
 #     比如: bin, script 等，需要根据场景修改
-__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
+#__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
 
 
 ###########################################
@@ -50,6 +50,8 @@ front_branch=master
 guomi_model=no
 # bcos 的 Docker 镜像版本
 bcos_image_tag="v2.2.0"
+# FISCO-BCOS 和 WeBASE-Front 镜像的 Docker Hub repository
+docker_repository="fiscoorg/front"
 
 # 解析参数
 __cmd="$(basename $0)"
@@ -57,18 +59,19 @@ __cmd="$(basename $0)"
 usage() {
     cat << USAGE  >&2
 Usage:
-    ${__cmd}    [-t new_tag] [-a git-account] [-c bcos_version] [-b front-branch] [-g] [-h]
+    ${__cmd}    [-t new_tag] [-c bcos_version] [-a git_account] [-b front_branch] [-p docker_repository] [-g] [-h]
     -t          Docker image new_tag, required.
-
-    -c          BCOS docker image new_tag, default v2.2.0, equal to fiscoorg/fiscobcos:v2.2.0.
+    
+    -c          BCOS docker image tag, default v2.4.0, equal to fiscoorg/fiscobcos:v2.4.0.
     -a          Git account, default WeBankFinTech.
-    -g          Use guomi, default no.
     -b          Branch of WeBASE-Front, default master.
+    -p          Which repository new image will be pushed to, default fiscoorg/front.
+    -g          Use guomi, default no.
     -h          Show help info.
 USAGE
     exit 1
 }
-while getopts t:c:a:b:gh OPT;do
+while getopts t:c:a:b:p:gh OPT;do
     case $OPT in
         t)
             new_tag=${OPTARG}
@@ -84,6 +87,9 @@ while getopts t:c:a:b:gh OPT;do
             ;;
         b)
             front_branch=${OPTARG}
+            ;;
+        p)
+            docker_repository=${OPTARG}
             ;;
         h)
             usage
@@ -102,36 +108,26 @@ if [[ "${new_tag}"x == "x" ]] ; then
   usage
   exit 1
 fi
-use_guomi=""
+
 if [[ "${guomi_model}"x == "yesx" ]] ; then
   # 如果是国密，检查 BCOS 的docker 镜像是否是 -结尾 gm
   if [[ ${bcos_image_tag} != *-gm ]] ; then
     LOG_WARN "BCOS docker image:[${bcos_image_tag}] should end with [-gm] when use guomi model."
     exit 1;
   fi
-
-  ## set front gradle build param
-  use_guomi=" -Pguomi "
 fi
-
 
 # 拉取 WeBASE-Front
 WEBASE_FRONT_GIT="https://github.com/${git_account}/${PROJECT_NAME}.git";
 LOG_INFO "git pull WeBASE-Front's branch: [${front_branch}] from ${WEBASE_FRONT_GIT}"
-git clone -b "${front_branch}" ${WEBASE_FRONT_GIT} --depth=1
+git clone -b "${front_branch}" "${WEBASE_FRONT_GIT}" --depth=1
 
 # 使用国密编译
-
-if [[ $(command -v gradle) ]]; then
-  # install ufw
-  cd "${PROJECT_NAME}" && gradle clean build -x test ${use_guomi} && cd ..
-else
-  cd "${PROJECT_NAME}" && chmod +x ./gradlew && ./gradlew clean build -x test ${use_guomi} && cd ..
-fi
-
+cd "${PROJECT_NAME}" && chmod +x ./gradlew && ./gradlew clean build -x test && cd ..
 rm -rfv ./dist &&  mv -fv ${PROJECT_NAME}/dist . && rm -rf ${PROJECT_NAME}
 mv -fv dist/conf_template dist/conf
 
+# 修改application.yml 配置
 if [[ "${guomi_model}"x == "yesx" ]] ; then
   sed -i "s/encryptType.*#/encryptType: 1 #/g" dist/conf/application.yml
 else
@@ -139,8 +135,8 @@ else
 fi
 
 LOG_INFO "Docker image new_tag is [${new_tag}]"
-docker build --build-arg BCOS_IMG_VERSION=${bcos_image_tag} -t front:${new_tag} .
-docker tag  front:${new_tag} fiscoorg/front:${new_tag}
+docker build --build-arg BCOS_IMG_VERSION="${bcos_image_tag}" -t front:"${new_tag}" .
+docker tag  front:"${new_tag}" fiscoorg/front:"${new_tag}"
 
 rm -rf dist
-docker push fiscoorg/front:${new_tag}
+docker push "${docker_repository}":"${new_tag}"
