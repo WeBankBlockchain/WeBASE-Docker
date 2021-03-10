7036594e265489eeb33b4d3a5f522e10ee0c8453
codingcattwo <847701726@qq.com>
2021-03-10 18:11:45 +0800

    update node config

--- nodes-config/nodes/172.17.0.3/node0/start.sh
+++ nodes-config/nodes/172.17.0.3/node0/start.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 SHELL_FOLDER=$(cd $(dirname $0);pwd)
 
+LOG_ERROR() {
+    content=${1}
+    echo -e "\033[31m[ERROR] ${content}\033[0m"
+}
+
+LOG_INFO() {
+    content=${1}
+    echo -e "\033[32m[INFO] ${content}\033[0m"
+}
+
 fisco_bcos=${SHELL_FOLDER}/../fisco-bcos
 cd ${SHELL_FOLDER}
 node=$(basename ${SHELL_FOLDER})
 node_pid=$(docker ps |grep ${SHELL_FOLDER//\//} | grep -v grep|awk '{print $1}')
-if [ ! -z ${node_pid} ];then
-    echo " ${node} is running, container id is $node_pid. Trying to load new groups."
-    touch config.ini.append_group
+if [ -n "${node_pid}" ];then
+    echo " ${node} is running, container id is $node_pid."
     exit 0
-else 
-    docker run -d --rm --name ${SHELL_FOLDER//\//} -v ${SHELL_FOLDER}:/data --network=host -w=/data fiscoorg/fiscobcos:v2.0.0 -c config.ini &
+else
+    docker run -d --rm --name ${SHELL_FOLDER//\//} -v ${SHELL_FOLDER}:/data --network=host -w=/data fiscoorg/fiscobcos:v2.7.1 -c config.ini
     sleep 1.5
 fi
 try_times=4
@@ -19,7 +28,7 @@ while [ $i -lt ${try_times} ]
 do
     node_pid=$(docker ps |grep ${SHELL_FOLDER//\//} | grep -v grep|awk '{print $1}')
     success_flag=success
-    if [[ ! -z ${node_pid} && ! -z "${success_flag}" ]];then
+    if [[ -n "${node_pid}" && -n "${success_flag}" ]];then
         echo -e "\033[32m ${node} start successfully\033[0m"
         exit 0
     fi
--- nodes-config/nodes/172.17.0.3/node0/stop.sh
+++ nodes-config/nodes/172.17.0.3/node0/stop.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 SHELL_FOLDER=$(cd $(dirname $0);pwd)
 
+LOG_ERROR() {
+    content=${1}
+    echo -e "\033[31m[ERROR] ${content}\033[0m"
+}
+
+LOG_INFO() {
+    content=${1}
+    echo -e "\033[32m[INFO] ${content}\033[0m"
+}
+
 fisco_bcos=${SHELL_FOLDER}/../fisco-bcos
 node=$(basename ${SHELL_FOLDER})
 node_pid=$(docker ps |grep ${SHELL_FOLDER//\//} | grep -v grep|awk '{print $1}')
-try_times=10
+try_times=20
 i=0
 if [ -z ${node_pid} ];then
     echo " ${node} isn't running."
     exit 0
 fi
-[ ! -z ${node_pid} ] && docker kill ${node_pid} 2>/dev/null > /dev/null
+[ -n "${node_pid}" ] && docker kill ${node_pid} 2>/dev/null > /dev/null
 while [ $i -lt ${try_times} ]
 do
     sleep 0.6
