47254dfc92b51cf2882699f82d9e0d15d41ecd87
codingcattwo <847701726@qq.com>
2021-03-17 14:28:54 +0800

    update build docker shell

--- docker/build/build-with-cdn.sh
+++ docker/build/build-with-cdn.sh
@@ -46,15 +46,11 @@ latest_tag="latest"
 docker_repository="fiscoorg/fisco-webase"
 docker_push="no"
 
-# 指定 WeBASE-Front 账号（开发调试使用）
-git_account="WeBank"
-# WeBASE-Front 的分支
-front_branch=dev
 # webase front默认版本
 front_version="v1.4.2"
 
 # 父镜像 FISCO-BCOS 的版本
-bcos_image_tag="latest"
+bcos_image_tag="v2.7.2"
 
 # 解析参数
 __cmd="$(basename $0)"
@@ -64,11 +60,8 @@ usage() {
 Usage:
     ${__cmd}    [-t new_tag] [-c bcos_version] [-a git_account] [-b front_branch] [-r docker_repository] [-p] [-h]
     -t          Docker image new_tag, required.
-
     -c          BCOS docker image tag, default v2.4.0, equal to fiscoorg/fiscobcos:v2.4.0.
     -f          Front version to pull from cdn, default v1.4.2
-    -a          Git account, default WeBankFinTech.
-    -b          Branch of WeBASE-Front, default master.
     -r          Which repository new image will be pushed to, default fiscoorg/front.
     -p          Execute docker push, default no.
     -h          Show help info.
@@ -85,12 +78,6 @@ while getopts t:c:f:a:b:r:ph OPT;do
             ;;
         f)
             front_version=${OPTARG}
-            ;;
-        a)
-            git_account=${OPTARG}
-            ;;
-        b)
-            front_branch=${OPTARG}
             ;;
         r)
             docker_repository=${OPTARG}
@@ -116,28 +103,17 @@ if [[ "${new_tag}"x == "x" ]] ; then
   exit 1
 fi
 
-# FISCO-BCSO 的 docker 镜像是 -gm 结尾, 使用国密
-#encrypt_type="0"
-#if [[ ${bcos_image_tag} == *-gm ]] ; then
-  # update application.yml of WeBASE-Front
-#  encrypt_type="1"
-
-#  if [[ ${new_tag} != *-gm ]] ; then
-#    new_tag="${new_tag}-gm"
-#    latest_tag="${latest_tag}-gm"
-#  fi
-
-#  LOG_INFO "FISCO-BCOS docker image:[${bcos_image_tag}] ends with [-gm], use guomi model, new image tag:[${new_tag} and ${latest_tag}]"
-#fi
 
 # 拉取 WeBASE-Front
 wget https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/WeBASE/releases/download/${front_version}/webase-front.zip
 unzip webase-front.zip && mv webase-front dist && rm -f webase-front.zip
+
+
 #WEBASE_FRONT_GIT="https://gitee.com/${git_account}/${PROJECT_NAME}.git";
 #LOG_INFO "git pull WeBASE-Front's branch: [${front_branch}] from ${WEBASE_FRONT_GIT}"
 #git clone -b "${front_branch}" "${WEBASE_FRONT_GIT}" --depth=1
 
-# # 使用国密编译
+# # 编译
 #cd "${PROJECT_NAME}" && chmod +x ./gradlew && ./gradlew clean build -x test && cd ..
 #rm -rfv ./dist &&  mv -fv ${PROJECT_NAME}/dist . && rm -rf ${PROJECT_NAME}
 #mv -fv dist/conf_template dist/conf
@@ -146,9 +122,6 @@ unzip webase-front.zip && mv webase-front dist && rm -f webase-front.zip
 #mkdir dist/conf/solcjs
 #wget -P dist/conf/solcjs https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/WeBASE/download/solidity/v0.6.10.js
 #wget -P dist/conf/solcjs https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/WeBASE/download/solidity/v0.6.10-gm.js
-
-# 修改application.yml 配置
-#sed -i "s/encryptType.*#/encryptType: ${encrypt_type} #/g" dist/conf/application.yml
 
 new_image="${docker_repository}":"${new_tag}"
 sudo docker build -f Dockerfile --build-arg BCOS_IMG_VERSION="${bcos_image_tag}" -t "${new_image}" .
--- docker/build/docker-build.sh
+++ docker/build/docker-build.sh
@@ -47,14 +47,13 @@ docker_repository="fiscoorg/fisco-webase"
 docker_push="no"
 
 # 指定 WeBASE-Front 账号（开发调试使用）
+# github的是WeBankFinTech，gitee的是WeBank
 git_account="WeBank"
 # WeBASE-Front 的分支
 front_branch=master
-# webase front默认版本
-front_version="v1.4.2"
 
-# 父镜像 FISCO-BCOS 的版本
-bcos_image_tag="v2.4.0"
+# 父镜像 FISCO-BCOS 的版本(默认版本)
+bcos_image_tag="v2.7.2"
 
 # 解析参数
 __cmd="$(basename $0)"
@@ -64,9 +63,7 @@ usage() {
 Usage:
     ${__cmd}    [-t new_tag] [-c bcos_version] [-a git_account] [-b front_branch] [-r docker_repository] [-p] [-h]
     -t          Docker image new_tag, required.
-
     -c          BCOS docker image tag, default v2.4.0, equal to fiscoorg/fiscobcos:v2.4.0.
-    -f          Front version to pull from cdn, default v1.4.2
     -a          Git account, default WeBankFinTech.
     -b          Branch of WeBASE-Front, default master.
     -r          Which repository new image will be pushed to, default fiscoorg/front.
@@ -82,9 +79,6 @@ while getopts t:c:a:b:r:ph OPT;do
             ;;
         c)
             bcos_image_tag=${OPTARG}
-            ;;
-        f)
-            front_version=${OPTARG}
             ;;
         a)
             git_account=${OPTARG}
@@ -116,39 +110,20 @@ if [[ "${new_tag}"x == "x" ]] ; then
   exit 1
 fi
 
-# FISCO-BCSO 的 docker 镜像是 -gm 结尾, 使用国密
-encrypt_type="0"
-if [[ ${bcos_image_tag} == *-gm ]] ; then
-  # update application.yml of WeBASE-Front
-  encrypt_type="1"
 
-  if [[ ${new_tag} != *-gm ]] ; then
-    new_tag="${new_tag}-gm"
-    latest_tag="${latest_tag}-gm"
-  fi
+WEBASE_FRONT_GIT="https://gitee.com/${git_account}/${PROJECT_NAME}.git";
+LOG_INFO "git pull WeBASE-Front's branch: [${front_branch}] from ${WEBASE_FRONT_GIT}"
+git clone -b "${front_branch}" "${WEBASE_FRONT_GIT}" --depth=1
 
-  LOG_INFO "FISCO-BCOS docker image:[${bcos_image_tag}] ends with [-gm], use guomi model, new image tag:[${new_tag} and ${latest_tag}]"
-fi
-
-# 拉取 WeBASE-Front
- wget https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/WeBASE/releases/download/${front_version}/webase-front.zip
- unzip webase-front.zip && mv webase-front dist && rm -f webase-front.zip
-# WEBASE_FRONT_GIT="https://gitee.com/${git_account}/${PROJECT_NAME}.git";
-# LOG_INFO "git pull WeBASE-Front's branch: [${front_branch}] from ${WEBASE_FRONT_GIT}"
-# git clone -b "${front_branch}" "${WEBASE_FRONT_GIT}" --depth=1
-
-# # 使用国密编译
-# cd "${PROJECT_NAME}" && chmod +x ./gradlew && ./gradlew clean build -x test && cd ..
-# rm -rfv ./dist &&  mv -fv ${PROJECT_NAME}/dist . && rm -rf ${PROJECT_NAME}
-# mv -fv dist/conf_template dist/conf
+# 编译
+cd "${PROJECT_NAME}" && chmod +x ./gradlew && ./gradlew clean build -x test && cd ..
+rm -rfv ./dist &&  mv -fv ${PROJECT_NAME}/dist . && rm -rf ${PROJECT_NAME}
+mv -fv dist/conf_template dist/conf
 
 # # conf里增加sol 0.6支持
-# mkdir dist/conf/solcjs
-# wget -P dist/conf/solcjs https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/WeBASE/download/solidity/v0.6.10.js
-# wget -P dist/conf/solcjs https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/WeBASE/download/solidity/v0.6.10-gm.js
-
-# 修改application.yml 配置
-sed -i "s/encryptType.*#/encryptType: ${encrypt_type} #/g" dist/conf/application.yml
+mkdir dist/conf/solcjs
+wget -P dist/conf/solcjs https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/WeBASE/download/solidity/v0.6.10.js
+wget -P dist/conf/solcjs https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/WeBASE/download/solidity/v0.6.10-gm.js
 
 new_image="${docker_repository}":"${new_tag}"
 sudo docker build -f Dockerfile --build-arg BCOS_IMG_VERSION="${bcos_image_tag}" -t "${new_image}" .
